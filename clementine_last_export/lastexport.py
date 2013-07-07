6ef47675a2dcbbe66597eef97c9a77945ccd9049
Vincent Verdeil <vincent.verdeil@gmail.com>
2013-07-07 16:53:22 +0200

    Correct print to show it only when necessary

--- clementine_last_export/lastexport.py
+++ clementine_last_export/lastexport.py
@@ -223,7 +223,8 @@ def main(server, username, startpage, outfile, infotype='recenttracks', use_cach
             
             for line in already_imported_lines:
                 outfileobj.write(line)
-            print "Completed with already imported informations"
+            if already_imported_lines != []
+                print "Completed with already imported informations"
             outfileobj.close()
 
 if __name__ == "__main__":
