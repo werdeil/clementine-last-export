6d95387341833d8849b19fa8baf05a23651c57ad
Vincent Verdeil <vincent.verdeil@gmail.com>
2013-08-23 23:38:50 +0200

    Move elements in the gui to correct error on non Ubuntu distribution

--- clementine_last_export/gui.py
+++ clementine_last_export/gui.py
@@ -77,34 +77,34 @@ class ClemLastExportGui(QtGui.QMainWindow):
         ##Part import
         lbl_part_import = QtGui.QLabel('Information about the server', self)
         lbl_part_import.resize(200, 20)
-        lbl_part_import.move(15, 10)
+        lbl_part_import.move(15, 30)
         
         ###Server selection
         lbl_combo_server  = QtGui.QLabel('Select the server', self)
         lbl_combo_server.resize(120, 20)
-        lbl_combo_server.move(20, 40)        
+        lbl_combo_server.move(20, 60)        
         
         server_combo = QtGui.QComboBox(self)
         server_combo.addItem("last.fm")
         server_combo.addItem("libre.fm")
-        server_combo.move(140, 35)
+        server_combo.move(140, 55)
         server_combo.activated[str].connect(self.serverChanged)
         
         ###Server credentials
         lbl_username  = QtGui.QLabel('Username', self)
-        lbl_username.move(20, 70)
+        lbl_username.move(20, 90)
         field_username = QtGui.QLineEdit(self)
-        field_username.move(140, 70)
+        field_username.move(140, 90)
         self.username = field_username.textChanged[str].connect(self.usernameChanged)
         
         ###Part target
         # Defintition of the two radio buttons
         playcount_radio_button = QtGui.QRadioButton('Import playcount', self)
-        playcount_radio_button.resize(160,20)
-        playcount_radio_button.move(20, 120)
+        playcount_radio_button.resize(160, 20)
+        playcount_radio_button.move(20, 140)
         lovedtracks_radio_button = QtGui.QRadioButton('Import loved tracks', self)
-        lovedtracks_radio_button.resize(160,20)
-        lovedtracks_radio_button.move(20, 150)
+        lovedtracks_radio_button.resize(160, 20)
+        lovedtracks_radio_button.move(20, 170)
         
         #the playcount radio button is selected by default
         playcount_radio_button.toggle() 
@@ -119,12 +119,12 @@ class ClemLastExportGui(QtGui.QMainWindow):
                 
         ##Part options
         lbl_part_update = QtGui.QLabel('Options', self)
-        lbl_part_update.move(15, 180)
+        lbl_part_update.move(15, 200)
         
         #Checkbox to activate or not the backup of the database
         backup_checkbox = QtGui.QCheckBox('Backup database', self)
         backup_checkbox.resize(200,20)
-        backup_checkbox.move(20, 210)
+        backup_checkbox.move(20, 230)
         #Backup is activated by default
         backup_checkbox.toggle()
         backup_checkbox.stateChanged.connect(self.backupChanged)
@@ -132,14 +132,14 @@ class ClemLastExportGui(QtGui.QMainWindow):
         #Checkbox to activate the force of the update (see tooltip for more information)
         force_update_checkbox = QtGui.QCheckBox('Force update', self)
         force_update_checkbox.resize(200,20)
-        force_update_checkbox.move(20, 240)
+        force_update_checkbox.move(20, 250)
         force_update_checkbox.stateChanged.connect(self.forceUpdateChanged)
         force_update_checkbox.setToolTip('Check this box if you want to force the update\n - of loved tracks already rated at 4.5 stars\n - of playcounts higher locally than the one on the music server')
         
         #Checkbox to activate the use of a cache file
         use_cache_checkbox = QtGui.QCheckBox('Use cache file (if available)', self)
         use_cache_checkbox.resize(200,20)
-        use_cache_checkbox.move(20, 270)
+        use_cache_checkbox.move(20, 290)
         #Cache file is used by default        
         use_cache_checkbox.toggle()
         use_cache_checkbox.stateChanged.connect(self.useCacheChanged)
@@ -149,20 +149,19 @@ class ClemLastExportGui(QtGui.QMainWindow):
         self.progressbar.setMinimum(1)
         self.progressbar.setMaximum(100)
         self.progressbar.resize(260, 20)
-        self.progressbar.move(20, 300)
-        
+        self.progressbar.move(20, 320)        
         
         ###Run button
         update_button = QtGui.QPushButton('Run', self)
         update_button.setToolTip('Run the script')
         update_button.resize(update_button.sizeHint())
-        update_button.move(190, 130)  
+        update_button.move(190, 150)  
         update_button.clicked.connect(self.run_script)
         #Run button can be triggered by pressing the return key
         update_button.setShortcut(update_button.tr("Return"))        
         
         ##Global window
-        self.resize(300, 350)
+        self.resize(300, 370)
         self.center()
         self.setWindowTitle('Clementine Last Export')  
         self.setWindowIcon(QtGui.QIcon(':/myresources/clementine_last_export.png'))
